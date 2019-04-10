b521d634f53c32346f68b69d7d8f4fda4c9403ba
ADRIAN REN <adrian.ren@outlook.com>
2019-04-11 09:47:15 +1200

    Updates

---
+++ precache-manifest.f284f5b1c3896d5509f3fca3ea1714f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d7fabd7db20e9ac5574",
+    "url": "/200Watts/static/css/main.a7783e3e.chunk.css"
+  },
+  {
+    "revision": "9d7fabd7db20e9ac5574",
+    "url": "/200Watts/static/js/main.47c77414.chunk.js"
+  },
+  {
+    "revision": "2350924be16b436d8da8",
+    "url": "/200Watts/static/js/runtime~main.7862c2c1.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/200Watts/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/200Watts/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eeb0db13db563d06ce7107253812b77a",
+    "url": "/200Watts/index.html"
+  }
+];
